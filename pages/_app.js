90bc5fe60e79453784e6232b195c75fc9e26fdd5
Jack Clarke <hello@idealpress.org>
2023-03-31 12:20:57 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Script from "next/script";
 import { PrismicProvider } from "@prismicio/react";
 import { PrismicPreview } from "@prismicio/next";
 
@@ -9,21 +10,37 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <PrismicProvider
-      linkResolver={linkResolver}
-      internalLinkComponent={({ href, children, ...props }) => (
-        <Link href={href}>
-          <a {...props}>{children}</a>
-        </Link>
-      )}
-    >
-      <PrismicPreview repositoryName={repositoryName}>
-        <MenuProvider>
-          <ThemeProvider>
-            {getLayout(<Component {...pageProps} />)}
-          </ThemeProvider>
-        </MenuProvider>
-      </PrismicPreview>
-    </PrismicProvider>
+    <>
+      <PrismicProvider
+        linkResolver={linkResolver}
+        internalLinkComponent={({ href, children, ...props }) => (
+          <Link href={href}>
+            <a {...props}>{children}</a>
+          </Link>
+        )}
+      >
+        <PrismicPreview repositoryName={repositoryName}>
+          <MenuProvider>
+            <ThemeProvider>
+              {getLayout(<Component {...pageProps} />)}
+            </ThemeProvider>
+          </MenuProvider>
+        </PrismicPreview>
+      </PrismicProvider>
+      <Script 
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-V64Z5SPNX7"
+        strategy="afterInteractive" 
+      />
+      <Script strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-V64Z5SPNX7');
+        `}
+      </Script>
+    </>
   )
 }
